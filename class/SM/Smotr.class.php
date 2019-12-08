c41345a002cb5e7e77f9f3b3f14faa5357587e2c
Caoimhín Ó Donnaíle <caoimhin@smo.uhi.ac.uk>
2019-12-08 16:01:15 +0000

    Atharraich an cookie airson cànan eadar-aghaidh bho smohl gu Thl

--- class/SM/Smotr.class.php
+++ class/SM/Smotr.class.php
@@ -2,15 +2,15 @@
 class SM_Smotr {
 
   public static function smotrHomeDir() {
-      if ($_SERVER['SERVER_NAME']=='www2.smo.uhi.ac.uk') { return '/teanga/smotr_dev'; }
-      return '/teanga/smotr';
+      if ($_SERVER['SERVER_NAME']=='www2.smo.uhi.ac.uk') { return '/teanga/smotr_dev/'; }
+      return '/teanga/smotr/';
   }
   public static function smotrUrl() {
       return 'https://' . $_SERVER['SERVER_NAME'] . self::smotrHomeDir();
   }
 
   public static function navbar($domhan='',$duilleagAghaidh=0,$str=0) {
-      $smohl = SM_T::hl0();
+      $hl0 = SM_T::hl0();
       $smotrHomeDir = self::smotrHomeDir();
       $smotrUrl     = self::smotrUrl();
       $T = new SM_T('smotr/navbar');
@@ -21,14 +21,14 @@ class SM_Smotr {
       $T_tr_fios            = $T->h('tr_fios');
       if ($duilleagAghaidh) { $SmotrCeangal = "<li><a href='/toisich/' title='Sabhal Mór Ostaig - prìomh dhuilleag (le dà briog)'>SMO</a>"; }
         else                { $SmotrCeangal = "<li><a href='$smotrHomeDir' title='$T_SmotrTitle'>Smotr</a>"; }
-      $strCeangal = "$smotrHomeDir/tr.php?tra=[$str]";
+      $strCeangal = "{$smotrHomeDir}tr.php?tra=[$str]";
       $strCeangal = ( $str ? "<li><a href='$strCeangal'>Sreang $str</a>" : '' );
       $myCLIL = SM_myCLIL::singleton();
       if ($myCLIL->cead(SM_myCLIL::LUCHD_EADARTHEANGACHAIDH) && !empty($domhan))
         { $trPutan = "\n<li class=deas><a href='/teanga/smotr/tr.php?domhan=$domhan' target='tr' title='$T_tr_fios'>tr</a>"; } else { $trPutan = ''; }
       $ceangalRiMoSMO = ( isset($myCLIL->id)
-                        ? "<li class='deas'><a href='$smotrHomeDir/logout.php' title='Log out from myCLIL'>Logout</a></li>"
-                        : "<li class='deas'><a href='$smotrHomeDir/login.php?till_gu=$smotrUrl/' title='$T_Log_air_fios'>$T_Log_air</a></li>"
+                        ? "<li class='deas'><a href='{$smotrHomeDir}logout.php' title='Log out from myCLIL'>Logout</a></li>"
+                        : "<li class='deas'><a href='{$smotrHomeDir}login.php?till_gu=$smotrUrl' title='$T_Log_air_fios'>$T_Log_air</a></li>"
                         );
       $hlArr = array(
           'br'=>'Brezhoneg',
@@ -48,12 +48,12 @@ class SM_Smotr {
       $options = '';
       foreach ($hlArr as $hl=>$hlAinm) {
           if (substr($hl,0,4)=='----') { $options .= "<option value='' disabled>&nbsp;_{$hlAinm}_</option>/n"; }  //Divider in the list of select options
-            else                       { $options .= "<option value='$hl|en'" . ( $hl==$smohl ? ' selected' : '' ) . ">$hlAinm</option>\n"; }
+            else                       { $options .= "<option value='$hl|en'" . ( $hl==$hl0 ? ' selected' : '' ) . ">$hlAinm</option>\n"; }
       }
       $selCanan = <<< END_selCanan
 <script>
     function atharraichCanan(hl) {
-        document.cookie='smohl='+hl;
+        document.cookie='Thl=' + hl + ';path={$smotrHomeDir};max-age=15000000';  //Math airson sia mìosan
         var paramstr = location.search;
         if (/Trident/.test(navigator.userAgent) || /MSIE/.test(navigator.userAgent)) {
           //Rud lag lag airson seann Internet Explorer, nach eil eòlach air URLSearchParams. Sguab ás nuair a bhios IE marbh.
